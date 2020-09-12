801fd72226f5f2d85852daf5b1ee0f9ce46e78c7
andreFSilva <andrealekhine@msn.com>
2020-09-11 22:12:34 -0300

    reinstall project

--- heroku/urls.py
+++ heroku/urls.py
@@ -1,4 +1,4 @@
-"""sysHeroku URL Configuration
+"""heroku URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,21 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView
-from clientes import urls as clientes_urls
-from home import urls as home_urls
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(home_urls)),
-    path('clientes/', include(clientes_urls)),
-    path('login/', LoginView.as_view(), name='login'),
-
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- heroku/wsgi.py
+++ heroku/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sysHeroku project.
+WSGI config for heroku project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sysHeroku.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heroku.settings.base')
 
 application = get_wsgi_application()
